8d11b1ad42c6279f8ec3f4627770513e0e45790e
Mateus <teteusao77@gmail.com>
2024-08-01 12:55:02 -0300

    commit test using logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { close, logo, menu } from "../assets";
+import { close, logo, logomemorias, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
@@ -7,7 +7,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      <img src={logomemorias} alt="hoobank" className="w-[124px] h-[32px]" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
