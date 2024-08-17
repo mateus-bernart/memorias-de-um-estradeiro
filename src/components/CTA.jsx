4c6039407288c5b50b5574eec923acdd93ffb0ec
Mateus <teteusao77@gmail.com>
2024-08-17 10:05:38 -0300

    made the cta more dinamic

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -9,24 +9,31 @@ const CTA = () => (
   >
     <div className="flex-1 flex flex-col">
       <h2 className={styles.heading2}>
-        Pague com <span className="text-gradient">pix</span> de forma ágil pelo
-        nosso <span className="green-gradient">Whatsapp</span>!
+        Pague com <span className="text-gradient">pix</span> e ganhe frete
+        grátis pelo nosso <span className="green-gradient">Whatsapp</span>!
       </h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        E tem mais: o frete é por nossa conta!
-      </p>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5 text-gradient`}>
-        Clique no Ícone do Whatsapp ao lado
-      </p>
+
+      {/* <p
+        className={`font-poppins font-normal text-dimWhite text-[35px] leading-[30.8px] max-w-[550px] mt-5 text-gradient`}
+      >
+        Clique no Ícone do Whatsapp!
+      </p> */}
     </div>
 
-    <div className={`sm:ml-10 ml-0 sm:mt-0 mt-5`}>
-      <a href="">
+    <div className={`sm:ml-10 ml-0 sm:mt-0 mt-5 mb-5`}>
+      <a href="https://wa.link/xej2fy">
         <img
           src={wppgreen}
           className={`w-[100px] h-[100px] object-contain cursor-pointer`}
           onClick={() => (window.location.href = socialMedia.link)} // Assuming "link" is the property that contains the URL
         />
+
+        <button
+          type="button"
+          className={`py-4 px-6 bg-green-600 font-poppins font-medium text-[25px] text-white outline-none ${styles} rounded-[8px]`}
+        >
+          Chamar agora!
+        </button>
       </a>
     </div>
   </section>
